7c8140accb23df3df2385dfd512b60e322f758f5
josephptflanagan <josephptflanagan@gmail.com>
2020-09-20 12:54:22 -0500

    initial commit

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique:true,
+            required: true,
+            trim: true
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v){
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                }
+            }
+
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+        
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// create the Pizza model using the PizzaSchema
+const User = model('User', UserSchema);
+
+// export the Pizza model
+module.exports = User;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { addThought, removeThought, addReaction, removeReaction } = require('../../controllers/thought-controller');
+
+// /api/comments/<userId>
+router.route('/:userId').post(addThought);
+
+//api/comments/<userId>/<thoughtId>
+router.route('/:userId/:thoughtId').put(addReaction).delete(removeThought);
+
+//api/comments/<userId>/<thoughtId>/<reactionId>
+router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
+
+module.exports = router;
